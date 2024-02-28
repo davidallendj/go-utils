c67dbccae819d10962974bf0df44580d5b29c039
David J. Allen <davidallendj@gmail.com>
2024-02-27 20:20:59 -0700

    Removed redundant HTTP method constants

--- httpx/httpx.go
+++ httpx/httpx.go
@@ -12,18 +12,6 @@ type Header map[string]string
 type Body []byte
 type Method = string
 
-const (
-	METHOD_GET     Method = "GET"
-	METHOD_POST    Method = "POST"
-	METHOD_PUT     Method = "PUT"
-	METHOD_PATCH   Method = "PATCH"
-	METHOD_DELETE  Method = "DELETE"
-	METHOD_HEAD    Method = "HEAD"
-	METHOD_CONNECT Method = "CONNECT"
-	METHOD_OPTIONS Method = "OPTIONS"
-	METHOD_TRACE   Method = "TRACE"
-)
-
 func MakeHTTPRequest(url string, httpMethod Method, body Body, headers Header) (*http.Response, []byte, error) {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	req, err := http.NewRequest(httpMethod, url, bytes.NewBuffer(body))
